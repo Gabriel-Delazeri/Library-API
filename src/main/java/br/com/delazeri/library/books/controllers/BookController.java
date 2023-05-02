ae8c21a8f69ec60d2808fd0092cb9ea63a7ff026
Gabriel-Delazeri <gbldelazeri@gmail.com>
2023-05-02 17:10:57 -0300

    feat: swagger docs from book's controller

--- src/main/java/br/com/delazeri/library/books/controllers/BookController.java
+++ src/main/java/br/com/delazeri/library/books/controllers/BookController.java
@@ -2,7 +2,14 @@ package br.com.delazeri.library.books.controllers;
 
 import br.com.delazeri.library.books.dtos.BookDTO;
 import br.com.delazeri.library.books.services.BookService;
+import br.com.delazeri.library.person.dtos.v1.PersonDTO;
 import br.com.delazeri.library.util.MediaType;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +18,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/books")
+@Tag(
+        name = "Book",
+        description = "Endpoints for Managing Books"
+)
 public class BookController {
 
     @Autowired
@@ -23,6 +34,27 @@ public class BookController {
                     MediaType.APPLICATION_YML
             }
     )
+    @Operation(
+            summary = "Finds all Books",
+            description = "Finds all Books",
+            tags = { "Books" },
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = BookDTO.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+
+            }
+    )
     public ResponseEntity<List<BookDTO>> getAllBooks() {
         return ResponseEntity.ok().body(service.getAll());
     }
@@ -33,6 +65,24 @@ public class BookController {
                     MediaType.APPLICATION_JSON,
                     MediaType.APPLICATION_XML,
                     MediaType.APPLICATION_YML
+            }
+    )
+    @Operation(
+            summary = "Finds a Book",
+            description = "Finds a Book",
+            tags = {"Books"},
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = BookDTO.class))
+                    ),
+                    @ApiResponse(description = "No content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+
             }
     )
     public ResponseEntity<BookDTO> findById(@PathVariable Long id) {
@@ -51,6 +101,21 @@ public class BookController {
                     MediaType.APPLICATION_YML
             }
     )
+    @Operation(
+            summary = "Adds a new Book",
+            description = "Adds a new Book by passing in a JSON, XML or YML representation of Book",
+            tags = {"Books"},
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = BookDTO.class))
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            }
+    )
     public ResponseEntity<BookDTO> create(@RequestBody BookDTO bookDTO) {
         return ResponseEntity.ok().body(service.create(bookDTO));
     }
@@ -67,11 +132,39 @@ public class BookController {
                     MediaType.APPLICATION_YML
             }
     )
+    @Operation(
+            summary = "Updates a Book",
+            description = "Updates a Book by passing in a JSON, XML or YML representation of Book",
+            tags = {"Books"},
+            responses = {
+                    @ApiResponse(
+                            description = "Success",
+                            responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = BookDTO.class))
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            }
+    )
     public ResponseEntity<BookDTO> update(@RequestBody BookDTO bookDTO) {
         return ResponseEntity.ok().body(service.update(bookDTO));
     }
 
     @DeleteMapping(value = "{id}")
+    @Operation(
+            summary = "Delete a Book",
+            description = "Delete a Book passing the id",
+            tags = {"Books"},
+            responses = {
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
+            }
+    )
     public ResponseEntity<?> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
